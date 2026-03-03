61c2caec3803e9c0b8d2189c5a8aa0ac18570613
Kminseo <kmss1105@gmail.com>
2026-03-03 14:07:43 +0900

    :card_file_box: refactor: change members' password
    fe의 비밀번호 기준에 맞추어 변경하였습니다.

--- BE/persona/src/main/resources/data.sql
+++ BE/persona/src/main/resources/data.sql
@@ -1,16 +1,16 @@
 -- 1. ROLE_USER (일반 사용자) 2명 생성
--- 비밀번호 '1234'
+-- 비밀번호 'user123'
 INSERT INTO member (id, username, password, email, role, birth, sex, is_creator, created_at, updated_at, is_active,status)
 VALUES
-    (nextval('member_sequence'), 'user01', '{bcrypt}$2a$10$FTNkrY7VKsf3rt5d1dNwBeRPscViiolO7LT4a7VUgN/Y8jiUdadlC', 'user01@example.com', 'ROLE_USER', '2001-05-15', 'FEMALE', true, now(), now(), true,'ACTIVE'),
-    (nextval('member_sequence'), 'user02', '{bcrypt}$2a$10$FTNkrY7VKsf3rt5d1dNwBeRPscViiolO7LT4a7VUgN/Y8jiUdadlC', 'user02@example.com', 'ROLE_USER', '2000-11-20', 'FEMALE', false, now(), now(), true,'ACTIVE');
+    (nextval('member_sequence'), 'user01', '{bcrypt}$2a$10$QQfcyom9v7jOr4/4OjnC/.UcP1k7TtAjEwNvj4D7yaPb26Q0NUstC', 'user01@example.com', 'ROLE_USER', '2001-05-15', 'FEMALE', true, now(), now(), true,'ACTIVE'),
+    (nextval('member_sequence'), 'user02', '{bcrypt}$2a$10$QQfcyom9v7jOr4/4OjnC/.UcP1k7TtAjEwNvj4D7yaPb26Q0NUstC', 'user02@example.com', 'ROLE_USER', '2000-11-20', 'FEMALE', false, now(), now(), true,'ACTIVE');
 
 -- 2. ROLE_ADMIN (관리자) 2명 생성
--- 비밀번호 'admin'
+-- 비밀번호 'admin123'
 INSERT INTO member (id, username, password, email, role, birth, sex, is_creator, created_at, updated_at, is_active, status)
 VALUES
-    (nextval('member_sequence'), 'admin', '{bcrypt}$2a$10$eQswQEePE9YBRlCvoGjzvucjQZ1JJYdhVwob.gfLJ5I.swOpReeDC', 'admin01@example.com', 'ROLE_ADMIN', '2002-03-10', 'FEMALE', true, now(), now(), true,'ACTIVE'),
-    (nextval('member_sequence'), 'admin1', '{bcrypt}$2a$10$eQswQEePE9YBRlCvoGjzvucjQZ1JJYdhVwob.gfLJ5I.swOpReeDC', 'admin02@persona.com', 'ROLE_ADMIN', '1995-01-01', 'MALE', false, now(), now(), true,'ACTIVE');
+    (nextval('member_sequence'), 'admin', '{bcrypt}$2a$10$OemF88kjqHcEo4jvSGqbKOejC/xK4Ff00LwycraTQCPEv/80JeKpm', 'admin01@example.com', 'ROLE_ADMIN', '2002-03-10', 'FEMALE', true, now(), now(), true,'ACTIVE'),
+    (nextval('member_sequence'), 'admin1', '{bcrypt}$$2a$10$OemF88kjqHcEo4jvSGqbKOejC/xK4Ff00LwycraTQCPEv/80JeKpm', 'admin02@persona.com', 'ROLE_ADMIN', '1995-01-01', 'MALE', false, now(), now(), true,'ACTIVE');
 
 -- 1. Persona 기본 정보 삽입 (member_id 10000, 10001 사용)
 INSERT INTO persona (id, name, profile, member_id, is_saved, created_at, updated_at, is_active, code, thumbnail)
